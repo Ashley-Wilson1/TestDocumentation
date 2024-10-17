7bc3fb8ea77530fbd9e480a5c6595745bf23bb2c
Ashley-Wilson1 <Ashley03wilson@gmail.com>
2024-10-17 17:44:14 +0100

    Added a compare method with correct Documentation

---
+++ TestDocumentation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TestDocumentation
+{
+    /// <summary>
+    /// Program is a generic class that doesn't do much.
+    /// by Ashley Wilson
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The entry point to the program.
+        /// </summary>
+        /// <param name="args">Array of strings from the command line.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        /// <summary>
+        /// Compare two integers and outputs the biggest, zero if equal.
+        /// </summary>
+        /// <param name="number1">First number to compare.</param>
+        /// <param name="number2">Second number to compare.</param>
+        /// <returns></returns>
+        public int compare(int number1, int number2)
+        {
+            if (number1 > number2)
+                return number1;
+            else if (number1 < number2)
+                return number2;
+            else
+                return 0;
+        }
+
+    }
+}
